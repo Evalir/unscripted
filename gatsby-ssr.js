ce61a240a3319a0e7ed4eda83cd9c7d281fdc3cd
Enrique Ortiz <hi@enriqueortiz.dev>
2021-02-13 17:39:38 -0400

    ssr: load correct font

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'normalize.css'
-import '@fontsource/source-sans-pro'
+import '@fontsource/inter'
 import Layout from './src/components/layout/layout'
 
 export function wrapPageElement({ element, props }) {
